3b2c11fc41ac2608f7d1c199f4fac98c2b890e8a
quantm.lqd@gmail.com <quantm.lqd@gmail.com>
2022-10-15 17:36:43 +0700

    fix getfactorial

--- src/com/tmquan/mathutil/core/MathUtil.java
+++ src/com/tmquan/mathutil/core/MathUtil.java
@@ -30,7 +30,7 @@ public class MathUtil {
             return 1;
         }
 
-        long product = 10;
+        long product = 1;
         for (int i = 2; i <= n; i++) {
             product *= i;
         }
